aa3d929de155a36bb17d7f4a38486727f94f619a
jstaffans <johannes.staffans@gmail.com>
2016-05-04 23:00:42 +0200

    Add cljs devtools

--- dev/cljs/user.cljs
+++ dev/cljs/user.cljs
@@ -1,8 +1,12 @@
 (ns cljs.user
-  (:require [figwheel.client :as figwheel]))
+  (:require [bild-ord.endpoint.app :refer [main]]
+            [devtools.core :as devtools]
+            [figwheel.client :as figwheel]))
 
 (js/console.info "Starting in development mode")
 
 (enable-console-print!)
+
+(devtools/install!)
 
 (figwheel/start {:websocket-url "ws://localhost:3449/figwheel-ws"})
