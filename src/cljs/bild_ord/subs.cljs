32973c23d990c10cc7ec62fb569ac9e587508976
jstaffans <johannes.staffans@gmail.com>
2016-05-28 20:38:13 +0200

    Comments

--- src/cljs/bild_ord/subs.cljs
+++ src/cljs/bild_ord/subs.cljs
@@ -2,11 +2,14 @@
   (:require-macros [reagent.ratom :refer [reaction]])
   (:require [re-frame.core :refer [register-sub]]))
 
+;; subscription for drop areas, to know whether or not
+;; a word has been dropped in the area
 (register-sub
  :dropped-word-query
  (fn [db [_ index]]
    (reaction (get-in @db [:answers index :word]))))
 
+;; returns the set of words already used in part 1 (already dragged)
 (register-sub
  :answers
  (fn [db _]
