007d7bad9901b20625ce164f150ec3066760e96a
jstaffans <johannes.staffans@gmail.com>
2016-05-31 09:05:34 +0200

    Comments, part 2 WIP

--- src/cljs/bild_ord/views.cljs
+++ src/cljs/bild_ord/views.cljs
@@ -1,7 +1,8 @@
 (ns bild-ord.views
-  (:require [re-frame.core :refer [subscribe]]
-            [bild-ord.views.common :as common]
-            [bild-ord.views.part-1 :as part-1]))
+  (:require [bild-ord.views.common :as common]
+            [bild-ord.views.part-1 :as part-1]
+            [bild-ord.views.part-2 :as part-2]
+            [re-frame.core :refer [subscribe]]))
 
 (defn render-container
   []
@@ -11,6 +12,7 @@
       ^{:key i} [common/render-illustration-svg i])]])
 
 (defn render-part-1
+  "Renders the first part of a game (dragging words to their correct slots)"
   []
   (conj
    (render-container)
@@ -25,10 +27,13 @@
        ^{:key i} [part-1/render-word i])]]))
 
 (defn render-part-2
+  "Renders the second part of a game (typing the words)"
   []
   (conj
    (render-container)
-   [:div.col.col-8.flex.flex-column.justify.around.fill-y]))
+   [:div.col.col-8.flex.flex-column.justify.around.fill-y
+    (for [i (range 5)]
+      ^{:key i} [part-2/render-input i])]))
 
 (defn app
   []
---
+++ src/cljs/bild_ord/views/part_2.cljs
@@ -0,0 +1,10 @@
+(ns bild-ord.views.part-2
+  (:require [bild-ord.domain.words :refer [words]]
+            [re-frame.core :refer [dispatch subscribe]]
+            [reagent.core :as reagent]))
+
+(defn render-input
+  "Renders one input where the user can type in a word"
+  [index]
+  [:div.flex.flex-wrap.content-end
+   [:div "Input"]])
