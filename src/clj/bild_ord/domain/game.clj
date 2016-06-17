f7a18b6a589ddde22e2b46fce201910f32a853e7
Robin Gower <robin.git@infonomics.ltd.uk>
2016-06-17 20:20:16 +0200

    game moves using clojure.spec and specter

--- src/clj/bild_ord/domain/game.clj
+++ src/clj/bild_ord/domain/game.clj
@@ -1,5 +1,6 @@
 (ns bild-ord.domain.game
-  (:require [clojure.spec :as s]))
+  (:require [clojure.spec :as s]
+            [com.rpl.specter :as specter]))
 
 (s/def ::word string?)
 
@@ -8,12 +9,13 @@
 (s/def ::slot (s/keys :req [::truth]
                       :opt [::guess]))
 (s/def ::slots (s/tuple ::slot ::slot ::slot ::slot ::slot))
+;; TODO: spec that each slot in slots should have different truth?
 
 (s/def ::used? boolean?)
 (s/def ::option (s/keys :req [::word]
                         :opt [::used?]))
 (s/def ::pile (s/tuple ::option ::option ::option ::option
-                       ::option ::option ::option ::option))
+                       ::option ::option ::option))
 
 (s/def ::group integer?)
 
@@ -22,3 +24,111 @@
 (s/def ::game (s/keys :req [::group ::slots ::current-stage]
                       :opt [::pile]))
 
+;; NB: need to check for nils here otherwise could compare nil with nil. Perhaps better to check that truth and guess are valid as either :pre condition or with s/fdef?
+(defn correct? [{:keys [::truth ::guess] :as slot}]
+  (and (not (nil? truth))
+       (not (nil? guess))
+       (= truth guess)))
+
+(defn success? [{:keys [::slots] :as game}]
+  {:pre [(s/valid? ::slots slots)]}
+  (every? correct? slots))
+
+(defn slot
+  ([truth]
+   {:post [(s/valid? ::slot %)]}
+   {::truth truth})
+  ([truth guess]
+   {:post [(s/valid? ::slot %)]}
+   {::truth truth ::guess guess}))
+
+(defn option
+  ([word]
+   {:post [(s/valid? ::option %)]}
+   {::word word
+    ::used? false}))
+
+(defn game
+  ([group slots current-stage]
+   {:post [(s/valid? ::game %)]}
+   {::group group
+    ::slots slots
+    ::current-stage current-stage})
+  ([group slots current-stage pile]
+   {:post [(s/valid? ::game %)]}
+   (assoc
+    (game group slots current-stage)
+    ::pile pile)))
+
+(defn new-game
+  ([group truths]
+   (game group
+         (into [] (map slot truths))
+         :typing))
+  ([group truths options]
+   (game group
+         (into [] (map slot truths))
+         :dragging
+         (into [] (map option options)))))
+
+
+(defn get-guess [game slot-index]
+  (-> game ::slots (nth slot-index) ::guess))
+
+(defn set-guess [game slot-index word]
+  {:post [(s/valid? ::game %)]}
+  (assoc-in game [::slots slot-index ::guess] word))
+
+(defn remove-guess [game slot-index]
+  (specter/transform [::slots (specter/keypath slot-index)]
+                     (fn [slot] (dissoc slot ::guess))
+                     game))
+
+
+(defn get-option [game word]
+  (->> game ::pile (filter #(= word (::word %))) first))
+
+(defn transform-option [game word transform-fn]
+  (specter/transform [::pile specter/ALL #(= (::word %) word)]
+                     transform-fn
+                     game))
+
+(defn set-used [option]
+  (assoc option ::used? true))
+
+(defn set-unused [option]
+  (assoc option ::used? false))
+
+(defn remove-option [game word]
+  {:post [(s/valid? ::game %)]}
+  (transform-option game word set-used))
+
+(defn restore-option [game word]
+  {:post [(s/valid? ::game %)]}
+  (transform-option game word set-unused))
+
+
+(defn guess-word [game slot-index word]
+  "Puts the word into the slot and removes the option from the pile"
+  {:pre [(s/valid? ::game game)
+         (integer? slot-index)
+         (s/valid? ::word word)]}
+  (-> game
+      (set-guess slot-index word)
+      (remove-option word)))
+
+(defn move-guess [game slot-index-from slot-index-to]
+  (-> game
+      (set-guess slot-index-to (get-guess game slot-index-from))
+      (remove-guess slot-index-from)))
+
+(defn cancel-guess [game slot-index]
+  (-> game
+      (restore-option (get-guess game slot-index))
+      (remove-guess slot-index)))
+
+(defn replace-guess [game slot-index word]
+  (-> game
+      (remove-option word)
+      (restore-option (get-guess game slot-index))
+      (set-guess slot-index word)))
---
+++ test/bild_ord/domain/game_test.clj
@@ -0,0 +1,92 @@
+(ns bild-ord.domain.game-test
+  (:require [clojure.test :refer :all]
+            [bild-ord.domain.game :refer :all :as game]))
+
+(def example-game
+  (new-game 0
+            ["lura" "sila" "vila" "gula" "måne"]
+            ["lura" "måne" "sila" "gula" "sola" "påse" "vila"]))
+
+(deftest play-dragging-game-test
+  (testing "right first time"
+    (is (success?
+         (-> example-game
+             (guess-word 0 "lura")
+             (guess-word 1 "sila")
+             (guess-word 2 "vila")
+             (guess-word 3 "gula")
+             (guess-word 4 "måne"))))))
+
+(deftest correctness-test
+  (testing "A slot may contain a correct guess"
+    (is (true? (correct? (slot "lura" "lura")))))
+  (testing "A slot may contain an incorrect guess"
+    (is (false? (correct? (slot "lura" "sila")))))
+  (testing "A slot with no guess is incorrect"
+    (is (false? (correct? (slot "lura")))))
+  (testing "A game is a collection of correctness"
+    (is (= [true false false]
+           (map correct? [(slot "lura" "lura")
+                          (slot "sila" "vila")
+                          (slot "vila" "sila")])))))
+
+(deftest success-test
+  (testing "A successful game"
+    (testing "has all answers correct"
+      (is (true? (success? {::game/slots [(slot "lura" "lura")
+                                          (slot "sila" "sila")
+                                          (slot "vila" "vila")
+                                          (slot "gula" "gula")
+                                          (slot "måne" "måne")]})))))
+  (testing "An unsuccessful game"
+    (testing "has some answers missing"
+      (is (false? (success? {::game/slots [(slot "lura" "lura")
+                                           (slot "sila" "sila")
+                                           (slot "vila")
+                                           (slot "gula")
+                                           (slot "måne")]}))))
+    (testing "has some answers incorrect"
+      (is (false? (success? {::game/slots [(slot "lura" "lura")
+                                           (slot "sila" "sila")
+                                           (slot "vila" "gula")
+                                           (slot "gula" "vila")
+                                           (slot "måne" "måne")]}))))))
+
+
+(deftest guess-word-test
+  (let [after-guess (guess-word example-game 0 "lura")]
+    (testing "puts the guess in the slot"
+      (is (= "lura"
+             (get-guess after-guess 0))))
+    (testing "removes the option from the pile"
+      (is (true? (::game/used? (get-option after-guess "lura")))))))
+
+(deftest move-guess-test
+  (let [after-move (-> example-game
+                       (guess-word 0 "lura")
+                       (move-guess 0 1))]
+    (testing "original guess has gone"
+      (is (nil? (get-guess after-move 0))))
+    (testing "guess is in new slot"
+      (is (= "lura"
+             (get-guess after-move 1))))))
+
+(deftest cancel-guess-test
+  (let [after-cancel (-> example-game
+                         (guess-word 0 "lura")
+                         (cancel-guess 0))]
+    (testing "original guess has gone"
+      (is (nil? (get-guess after-cancel 0))))
+    (testing "guess is back in pile"
+      (is (false? (::game/used? (get-option after-cancel "lura")))))))
+
+(deftest replace-guess-test
+  (let [after-replace (-> example-game
+                          (guess-word 0 "lura")
+                          (replace-guess 0 "sila"))]
+    (testing "original guess has been replaced with new guess"
+      (is (= "sila" (get-guess after-replace 0))))
+    (testing "original guess is back in the pile"
+      (is (false? (::game/used? (get-option after-replace "lura")))))
+    (testing "new guess is removed from pile"
+      (is (true? (::game/used? (get-option after-replace "sila")))))))
