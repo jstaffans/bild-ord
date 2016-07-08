01f1ceb0b10b85bafb11036e18da0519c22565a7
jstaffans <johannes.staffans@gmail.com>
2016-07-08 19:08:50 +0200

    Trick to avoid rendering UI before db is initialised

--- src/cljs/bild_ord/app.cljs
+++ src/cljs/bild_ord/app.cljs
@@ -4,7 +4,7 @@
             [bild-ord.routes :as routes]
             bild-ord.subs
             [bild-ord.views :as views]
-            [re-frame.core :refer [dispatch]]
+            [re-frame.core :refer [dispatch-sync]]
             [reagent.core :as reagent]))
 
 (defn mount-root
@@ -13,6 +13,6 @@
   (reagent/render [views/app] (.getElementById js/document "app")))
 
 (defn ^:export main []
-  (dispatch [:initialise-db])
+  (dispatch-sync [:initialise-db])
   (routes/init)
   (mount-root))
