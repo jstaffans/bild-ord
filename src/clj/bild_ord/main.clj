151719e41c3d4381079d30a9f0b491305d284ce0
jstaffans <johannes.staffans@gmail.com>
2016-07-19 13:08:48 +0300

    nREPL in production

--- src/clj/bild_ord/main.clj
+++ src/clj/bild_ord/main.clj
@@ -6,10 +6,12 @@
             [duct.util.runtime :refer [add-shutdown-hook]]
             [meta-merge.core :refer [meta-merge]]
             [bild-ord.config :as config]
-            [bild-ord.system :refer [new-system]]))
+            [bild-ord.system :refer [new-system]]
+            [nrepl.embed :as nrepl]))
 
 (def prod-config
-  {:app {:middleware     [[wrap-hide-errors :internal-error]]
+  {:db  {:connection-uri "jdbc:sqlite:/var/lib/sqlite/bild_ord"}
+   :app {:middleware     [[wrap-hide-errors :internal-error]]
          :internal-error (io/resource "errors/500.html")}})
 
 (def config
@@ -17,8 +19,15 @@
               config/environ
               prod-config))
 
+(def system (atom nil))
+
 (defn -main [& args]
-  (let [system (new-system config)]
-    (println "Starting HTTP server on port" (-> system :http :port))
-    (add-shutdown-hook ::stop-system #(component/stop system))
-    (component/start system)))
+  (reset! system (new-system config))
+
+  (println "Starting nREPL on port 7888")
+  (add-shutdown-hook ::stop-nrepl nrepl/stop-nrepl!)
+  (nrepl/start-nrepl! {:bind "127.0.0.1" :port 7888})
+
+  (println "Starting HTTP server on port" (-> @system :http :port))
+  (add-shutdown-hook ::stop-system #(swap! system component/stop))
+  (swap! system component/start))
