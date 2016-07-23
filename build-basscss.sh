897fd6ee44c1b1cbb780a8dec487f6838f1703b0
jstaffans <johannes.staffans@gmail.com>
2016-07-23 10:23:10 +0300

    Inline button styles

--- build-basscss.sh
+++ build-basscss.sh
@@ -2,6 +2,5 @@
 
 curl https://npmcdn.com/basscss@8.0.1/css/basscss.min.css > resources/bild_ord/public/css/base.css
 curl https://npmcdn.com/basscss-base-forms@2.0.2/index.css >> resources/bild_ord/public/css/base.css
-curl https://npmcdn.com/basscss-btn@1.1.1/index.css >> resources/bild_ord/public/css/base.css
 
 
