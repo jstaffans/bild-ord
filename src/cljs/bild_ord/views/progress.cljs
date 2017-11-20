ceed6201c0a80ec51ede6b43087bd586e8890963
Johannes Staffans <johannes.staffans@gmail.com>
2017-11-20 22:14:28 +0100

    css animation for progress

--- src/cljs/bild_ord/views/progress.cljs
+++ src/cljs/bild_ord/views/progress.cljs
@@ -1,28 +1,9 @@
 (ns bild-ord.views.progress
-  (:require cljsjs.jquery
-            [re-frame.core :refer [subscribe]]
+  (:require [re-frame.core :refer [subscribe]]
             [reagent.core :as reagent]))
 
-(defn animate-progress
-  [component]
-  (let [container (-> (js/$ (reagent/dom-node component)))
-        wrapper   (-> container (.children ".progress-wrap"))
-        bar       (-> wrapper (.children ".progress-bar"))
-        width     (.width wrapper)
-        progress  (* (/ (-> component reagent/props :percent) 100) width)]
-    (-> bar
-        (.stop)
-        (.animate #js {:width (str progress "px")}))))
-
 (defn progress
-  []
-  (reagent/create-class
-   {:reagent-render (fn []
-                      [:div.progress-container
-                       [:div.progress.progress-wrap
-                        [:div.progress.progress-bar]]])
-
-    :display-name "progress"
-
-    :component-did-mount  animate-progress
-    :component-did-update animate-progress}))
+  [{:keys [percent]}]
+  [:div.progress-container
+   [:div.progress.progress-wrap
+    [:div.progress.progress-bar {:class (str "percent-" percent)}]]])
